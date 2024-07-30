1f556847b79c22646a9a04c65322e8c6275e34b1
omardoescode <omardoescode@gmail.com>
2024-07-30 15:53:42 +0300

    Add main file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <filesystem>
+#include <iostream>
+
+int main (int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Invalid number of argument" << std::endl;
+        return 0;
+    }
+
+    std::filesystem::path inp = argv[1];
+    if (!std::filesystem::exists (inp)) {
+        std::cout << "Invalid file path" << std::endl;
+        return 0;
+    }
+
+    std::string out = std::filesystem::is_directory (inp) ?
+    inp.stem ().string () + ".xml" :
+    inp.stem ().string ().substr (0, inp.stem ().string ().length () - 5) + ".xml";
+
+    std::cout << "Writing to " << out << std::endl;
+}
