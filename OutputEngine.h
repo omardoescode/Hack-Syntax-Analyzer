e93d1b9bdfd90559d412317f5e04f6732708e114
omardoescode <omardoescode@gmail.com>
2024-08-01 11:12:11 +0300

    make the output engine abstract

--- OutputEngine.h
+++ OutputEngine.h
@@ -10,17 +10,17 @@ class OutputEngine {
 public:
     OutputEngine (const std::string&, std::shared_ptr<HackMap> hack_map);
 
-    virtual void write_keyword (const std::string&);
-    virtual void write_identifier (const std::string&);
-    virtual void write_symbol (const std::string&);
-    virtual void write_int_const (const std::string&);
-    virtual void write_string_const (const std::string&);
+    virtual void write_keyword (const std::string&)      = 0;
+    virtual void write_identifier (const std::string&)   = 0;
+    virtual void write_symbol (const std::string&)       = 0;
+    virtual void write_int_const (const std::string&)    = 0;
+    virtual void write_string_const (const std::string&) = 0;
 
-    virtual void write_non_terminal (const std::string&);
-    virtual void close_non_terminal ();
+    virtual void write_non_terminal (const std::string&) = 0;
+    virtual void close_non_terminal ()                   = 0;
 
 private:
-    virtual void write_terminal (const TokenType&, const std::string&);
+    virtual void write_terminal (const TokenType&, const std::string&) = 0;
 
 
 protected:
