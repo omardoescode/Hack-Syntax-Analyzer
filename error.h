69ce621c85835580ebc0ca2a47fe05f4601ab151
omardoescode <omardoescode@gmail.com>
2024-08-03 12:52:42 +0300

    update .clang-tidy

--- error.h
+++ error.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <exception>
 #include <string>
+#include <utility>
 class Error : public std::exception {
 public:
-    Error (const char* mesg) : message{ mesg } {
+    explicit Error (const char* mesg) : message{ mesg } {
     }
-    Error (std::string mesg) : message{ mesg } {
+    explicit Error (std::string mesg) : message{ std::move (mesg) } {
     }
 
-    const char* what () const throw () {
+    [[nodiscard]] const char* what () const noexcept override {
         return message.c_str ();
     }
 
